0bb1f5437a0ed2b0dedca2bdbbdf2e974f4bf2ea
Houssam-Eddine <94980688+HoussamEddineLamzoudi@users.noreply.github.com>
2022-12-15 14:53:47 +0100

    add model user

--- src/main/java/com/spring/Controller/AuthControler.java
+++ src/main/java/com/spring/Controller/AuthControler.java
@@ -1,6 +1,7 @@
 package com.spring.Controller;
 
 import com.spring.Service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class AuthControler {
 
-        AuthService authService = new AuthService();
+//        AuthService authService = new AuthService();
+    @Autowired
+    AuthService authService;
     @RequestMapping(value = "/login-index", method = RequestMethod.GET)
     public String index(){
         return "login";
---
+++ src/main/java/com/spring/Model/User.java
@@ -0,0 +1,73 @@
+package com.spring.Model;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "users", schema = "public", catalog = "spring")
+public class User {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private int id;
+    private String username;
+    private String email;
+    private String password;
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User user)) return false;
+        return id == user.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
